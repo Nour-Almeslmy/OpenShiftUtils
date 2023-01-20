3aeccc1b04b42a1e149e8dcca926074cdef6c8f8
Nour-Almeslmy <nouralmeslmy@gmail.com>
2023-01-21 00:18:22 +0200

    Add project files.

---
+++ AreasUtils/AerasDims.cs
@@ -0,0 +1,17 @@
+﻿namespace AreasUtils
+{
+    public class AerasDims
+    {
+
+        int Length { get; set; }
+        int Width { get; set; }
+
+        public decimal perimeter => (this.Length+this.Width)*2;
+
+        public AerasDims(int length, int width)
+        {
+            Length = length;
+            Width = width;  
+        }
+    }
+}
